07cc541e8d81b9e4106ccc1cf4a23257be927a9c
6ameDev <sumitdas.usit@gmail.com>
2025-04-08 04:03:23 +0530

    Added stream url route

--- app/routes/stream.py
+++ app/routes/stream.py
@@ -1,7 +1,38 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, HTTPException
+import logging
+from urllib.parse import urlparse, parse_qs
+import yt_dlp
 
 router = APIRouter()
 
+ydl_opts = {
+    'format': 'bestaudio',
+    'quiet': True,
+    'noplaylist': True,
+    'extract_flat': False,
+}
+ydl = yt_dlp.YoutubeDL(ydl_opts)
+
 @router.get("/stream-url")
-async def stream_url(track_id: str):
-    return {"url": f"https://yourcdn.com/stream/{track_id}"}
+async def stream_url(
+    videoId: str = Query(..., description="Video ID")
+):
+    try:
+      info = ydl.extract_info(f"https://music.youtube.com/watch?v={videoId}", download=False)
+      if not 'url' in info:
+          raise HTTPException(status_code=404, detail="Stream URL not found")
+
+      stream_url = info['url']
+
+      parsed_url = urlparse(stream_url)
+      query_params = parse_qs(parsed_url.query)
+      expire_timestamp = query_params.get("expire", [None])[0]
+
+      return {
+          "url": stream_url,
+          "expires_at": int(expire_timestamp) if expire_timestamp else None
+      }
+
+    except Exception as e:
+        logging.error(f"Error fetching stream url: {str(e)}", exc_info=True)
+        raise HTTPException(status_code=500, detail="Internal Server Error. Please try again later.")
